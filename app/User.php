515262a41bcb2d55cdc33ba7caf549df79eddc86
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-10-30 15:08:10 +0700

    use datetime

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use DateTime;
 
 class User extends Authenticatable implements JWTSubject
 {
