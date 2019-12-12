5cbeaf355a4fbef649c965ace1228cd355797f5d
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-12-12 13:52:23 +0700

    jos

--- gate_out.py
+++ gate_out.py
@@ -228,7 +228,7 @@ def gate_out_thread():
                         break
 
                     elif b'\xa6Y' in r:
-                        nomor_barcode = str(r).split('\xa6Y')[1].split('\\xa9')[0]
+                        nomor_barcode = str(r).split('Y')[1].split('\\xa9')[0]
                         access_log = get_last_access('nomor_barcode', str(int(nomor_barcode, 16)))
                         logging.info('Barcode detected : ' + str(int(nomor_barcode, 16)))
 
