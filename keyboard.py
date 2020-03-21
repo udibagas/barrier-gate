a8aa22f6d59183854b9cb6008b842ee0e7c2ff3b
Bagas Udi Sahsangka <udibagas@gmail.com>
2020-03-21 10:19:20 +0700

    update keybord

--- keyboard.py
+++ keyboard.py
@@ -5,8 +5,11 @@ import time
 from serial import Serial
 
 with Serial.open('/dev/ttyS1', 9600) as ser:
+    x = ''
     while True:
         # data will be *CD:ABC123#
         # xxxxxxxx = 8/14 digit in hex
-        x = ser.readline()
-        pyautogui.typewrite(str(int(x[4:][:-1], 16)))
+        x += ser.read()
+        if '#' in x:
+            pyautogui.typewrite(str(int(x[4:][:-1], 16)))
+        x = ''
