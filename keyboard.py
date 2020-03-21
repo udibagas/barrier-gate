4bda6dcc5ef91d4db68067389fc91b040d6cc92c
Bagas Udi Sahsangka <udibagas@gmail.com>
2020-03-21 10:24:33 +0700

    decode

--- keyboard.py
+++ keyboard.py
@@ -9,7 +9,7 @@ with Serial('/dev/ttyUSB0', 9600) as ser:
     while True:
         # data will be *CD:ABC123#
         # xxxxxxxx = 8/14 digit in hex
-        x += ser.read()
+        x += ser.read().decode('utf-8')
         if '#' in x:
             pyautogui.typewrite(str(int(x[4:][:-1], 16)))
         x = ''
